b324ab8094a420ddcc07b9cce5d00be352b160b3
ClassicTube <80894547+ClassicTube@users.noreply.github.com>
2021-10-05 20:51:31 +0200

    Add files via upload

---
+++ launcher.bat
@@ -0,0 +1,39 @@
+@echo off
+Title Mineczek Launcher
+:main
+set /p Name=<nickname.txt
+echo Your username is: %Name%
+echo.
+echo How to launch versions
+echo just type in the codenames below ex. a1.1.1
+echo Alpha 1.1.1 = a1.1.1
+echo Beta 1.7.3 = b1.7.3
+echo Beta 1.7.4 = b1.7.4
+echo Beta 1.7.5 = b1.7.5
+echo 1.5.2 = 1.5.2
+set /p version=
+if %version%==a1.1.1 goto launch_a111
+if %version%==b1.7.3 goto launch_b173
+if %version%==b1.7.4 goto launch_b174
+if %version%==b1.7.5 goto launch_b175
+if %version%==1.5.2 goto launch_152
+:launch_b173
+set /p Name=<nickname.txt
+java -Dhttp.proxyHost=betacraft.pl -Xmx1G -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump -Djava.net.preferIPv4Stack=true -cp "%CD%\versions\b1.7.3.jar;%CD%\bin\jinput.jar;%CD%\bin\lwjgl.jar;%CD%\bin\lwjgl_util.jar" "-Dorg.lwjgl.librarypath=%CD%\bin\natives" "-Dnet.java.games.input.librarypath=%CD%\bin\natives" net.minecraft.client.Minecraft %Name%
+goto main
+:end
+:launch_a111
+set /p Name=<nickname.txt
+java -Dhttp.proxyHost=betacraft.pl -Xmx1G -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump -Djava.net.preferIPv4Stack=true -cp "%CD%\versions\a1.1.1\a1.1.1.jar;%CD%\versions\a1.1.1\bin\jinput.jar;%CD%\versions\a1.1.1\bin\lwjgl.jar;%CD%\versions\a1.1.1\bin\lwjgl_util.jar" "-Dorg.lwjgl.librarypath=%CD%\versions\a1.1.1\bin\natives" "-Dnet.java.games.input.librarypath=%CD%\versions\a1.1.1\bin\natives" net.minecraft.client.Minecraft %Name%
+goto main
+:end
+:launch_b175
+set /p Name=<nickname.txt
+java -Dhttp.proxyHost=betacraft.pl -Xmx1G -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump -Djava.net.preferIPv4Stack=true -cp "%CD%\versions\b1.7.5.jar;%CD%\bin\jinput.jar;%CD%\bin\lwjgl.jar;%CD%\bin\lwjgl_util.jar" "-Dorg.lwjgl.librarypath=%CD%\bin\natives" "-Dnet.java.games.input.librarypath=%CD%\bin\natives" net.minecraft.client.Minecraft %Name%
+goto main
+:end
+:launch_b174
+set /p Name=<nickname.txt
+java -Dhttp.proxyHost=betacraft.pl -Xmx1G -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump -Djava.net.preferIPv4Stack=true -cp "%CD%\versions\b1.7.4.jar;%CD%\bin\jinput.jar;%CD%\bin\lwjgl.jar;%CD%\bin\lwjgl_util.jar" "-Dorg.lwjgl.librarypath=%CD%\bin\natives" "-Dnet.java.games.input.librarypath=%CD%\bin\natives" net.minecraft.client.Minecraft %Name%
+goto main
+:end
